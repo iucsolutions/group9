d42a4518917148a7643893b876032c390d2faa8f
iamgroup9 <iamgroup9>
2018-09-21 15:50:34 +0000

    Export: export goup9

--- IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsgroup9.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsgroup9.lua
@@ -1,4 +1,4 @@
-local groupName = 'GROUPNAME'
+local groupName = 'group9'
 local envVariable = 'serverType'..groupName
 local serverType = os.getenv(envVariable)
 
@@ -6,22 +6,22 @@ local iucConf = {}
 
 iucConf.dev = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/group9/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/group9/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
 
 iucConf.prod = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/prod/group9/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/prod/group9/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
